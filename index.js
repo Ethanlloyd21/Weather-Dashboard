2a25a4e0c2343ee7ac600f162dc8a29377290f22
Lloyd Marcelino <lloydmarcelino@gmail.com>
2020-01-11 06:52:12 -0800

    update

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 $(document).ready(function () {
 
     //setIcons(icon, document.getElementById('icon1'));
-
-
-
     var apiKey = "&appid=6ad58c387533b011c868da37071d9cec";
-    var city = "Seattle";
+
     var latt;
     var longg;
     getLocation();
 
+    //getting the user's location via geoLocation
     function getLocation() {
 
         if (navigator.geolocation) {
@@ -19,7 +17,7 @@ $(document).ready(function () {
             alert("Geolocation is not supported by this browser.");
         }
     }
-
+    //if the user is successfuly located via geoLocation
     function geoSuccess(position) {
         var latt = position.coords.latitude;
         var longg = position.coords.longitude;
@@ -29,19 +27,13 @@ $(document).ready(function () {
             type: "GET",
             dataType: "json",
             success: function (data) {
-                console.log(data);
-                console.log(data.main.temp);
-                console.log(data.weather[0].main);
-
-
                 var today = new Date(Date.now() + 1);
                 var date = today.toDateString();
-                console.log(today);
-                console.log(city);
+                //append result
                 $('#location').append(data.name);
                 $('#dateNow').append(date);
                 $('#con').append('Current condition: ' + data.weather[0].description);
-                $('#temp').append('Temperature: ' + data.main.temp + ' ' + String.fromCharCode(176) + 'F');
+                $('#temp').append('Temperature: ' + (parseInt(data.main.temp).toFixed(1)) + ' ' + String.fromCharCode(176) + 'F');
                 $('#humidity').append('Humidity: ' + data.main.humidity + '%');
                 $('#windSpeed').append('Wind Speed: ' + data.wind.speed + ' MPH');
 
@@ -52,7 +44,9 @@ $(document).ready(function () {
 
 
 
-
+    /*
+    *****Search Field*******
+    */
     $('#submitCity').click(function () {
         var city = $('#cityName').val();
 
@@ -61,34 +55,14 @@ $(document).ready(function () {
 
             $.ajax({
                 url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + apiKey,
-                type: "GET",
-                dataType: "json",
-                success: function (data) {
-                    console.log(data);
-                    console.log(data.main.temp);
-                    console.log(data.weather[0].main);
-
-
-                    var today = new Date(Date.now() + 1);
-                    var date = today.toDateString();
-                    console.log(today);
-                    console.log(city);
-                    $('#location').append(data.name);
-                    $('#dateNow').append(date);
-                    $('#con').append('Current condition: ' + data.weather[0].description);
-                    $('#temp').append('Temperature: ' + data.main.temp + ' ' + String.fromCharCode(176) + 'F');
-                    $('#humidity').append('Humidity: ' + data.main.humidity + '%');
-                    $('#windSpeed').append('Wind Speed: ' + data.wind.speed + ' MPH');
-
-
-                }
-
-
+                method: "GET"
+            }).then(function (response) {
+                console.log(response);
+                renderButtons(response.name);
             });
-
         }
         else {
-            $('#error').html('Search cannot be empty');
+            $('#error').html('Found error on search field');
         }
     });
     /*
@@ -99,6 +73,21 @@ $(document).ready(function () {
         return skycons.set(iconID, Skycons[currentIcon]);
     }
     */
+
+    function renderButtons(city) {
+        $('buttons-view').empty();
+        console.log(city);
+        var a = $("<button id='place'>");
+        // Adding a class of movie to our button
+        a.addClass('btn btn-dark');
+        // Adding a data-attribute
+        a.attr('data-name', city);
+        // Providing the initial button text
+        a.text(city);
+        // Adding the button to the buttons-view div
+        $('#buttons-view').append(a);
+        console.log(a);
+    }
 
     function currentTemp(city) {
 
