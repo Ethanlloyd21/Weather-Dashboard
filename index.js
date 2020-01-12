06e6d4b08d28f385065840ec3278de89513ba48d
Lloyd Marcelino <lloydmarcelino@gmail.com>
2020-01-11 22:27:44 -0800

    updage

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ $(document).ready(function () {
 
     var latt;
     var longg;
+    var cityNames = [];
+    var city = $('#cityName')
+        .val()
+        .trim();
+    cityNames.push(city);
     getLocation();
 
     //getting the user's location via geoLocation
@@ -27,8 +32,6 @@ $(document).ready(function () {
             type: "GET",
             dataType: "json",
             success: function (data) {
-
-
                 console.log(data);
                 console.log(data.coord.lat);
                 var today = new Date(Date.now());
@@ -37,16 +40,21 @@ $(document).ready(function () {
                 var lon = data.coord.lon;
                 console.log(lon);
                 //append result
-                $('#location').append(data.name);
-                $('#dateNow').append(date);
-                $('#con').append('Current condition: ' + data.weather[0].description);
-                $('#temp').append('Temperature: ' + (parseInt(data.main.temp).toFixed(1)) + ' ' + String.fromCharCode(176) + 'F');
-                $('#humidity').append('Humidity: ' + data.main.humidity + '%');
-                $('#windSpeed').append('Wind Speed: ' + data.wind.speed + ' MPH');
-                $('#uV').append('UV Index: ' + getUvIndex(lat, lon));
+                $('#location').text(data.name);
+                $('#dateNow').text(date);
+                $('#con').text('Current condition: ' + data.weather[0].description);
+                $('#temp').text('Temperature: ' + (parseInt(data.main.temp).toFixed(1)) + ' ' + String.fromCharCode(176) + 'F');
+                $('#humidity').text('Humidity: ' + data.main.humidity + '%');
+                $('#windSpeed').text('Wind Speed: ' + data.wind.speed + ' MPH');
+                updateUvIndex(lat, lon);
+                /*$('#uV').append('UV Index: ' + getUvIndex(lat, lon));*/
 
-                var info = getUvIndex(lat, lon);
-                console.log(info);
+                /*
+                getUvIndex(lat, lon).then(function (response) {
+                    console.log(response.value);
+
+                })*/
+
 
             }
         });
@@ -59,8 +67,11 @@ $(document).ready(function () {
     *****Search Field*******
     */
     $('#submitCity').click(function () {
-        var city = $('#cityName').val();
 
+        var city = $('#cityName')
+            .val()
+            .trim();
+        cityNames.push(city);
 
         if (city !== '') {
 
@@ -70,12 +81,28 @@ $(document).ready(function () {
             }).then(function (response) {
                 console.log(response);
                 renderButtons(response.name);
+                currentCity(city);
+                fiveDayFor(city);
             });
         }
         else {
-            $('#error').html('Found error on search field');
+            $('#error').html('Search field cannot be empty');
         }
     });
+
+    $('#place').click(function () {
+        var loc = $(this).attr("data-name");
+        $.ajax({
+            url: "http://api.openweathermap.org/data/2.5/weather?q=" + loc + "&units=imperial" + apiKey,
+            method: "GET"
+        }).then(function (response) {
+            currentCity(city);
+            fiveDayFor(city);
+        });
+
+    });
+
+
     /*
     function setIcons(icon, iconID) {
         var skycons = new Skycons({ color: "white"});
@@ -89,37 +116,37 @@ $(document).ready(function () {
         $('buttons-view').empty();
         console.log(city);
         var a = $("<button id='place'>");
-        // Adding a class of movie to our button
+
         a.addClass('btn btn-dark');
-        // Adding a data-attribute
+
         a.attr('data-name', city);
-        // Providing the initial button text
+
         a.text(city);
-        // Adding the button to the buttons-view div
+
         $('#buttons-view').append(a);
-        console.log(a);
+
     }
 
-    function currentTemp(city) {
+    function currentCity(city) {
 
         $.ajax({
             url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + apiKey,
             type: "GET",
             dataType: "json",
             success: function (data) {
-                console.log(data);
-                console.log(data.main.temp);
-                console.log(data.weather[0].main);
-                var today = new Date(Date.now() + 1);
+                var today = new Date(Date.now());
                 var date = today.toDateString();
-                console.log(today);
-                console.log(city);
-                $('#location').append(data.name);
-                $('#dateNow').append(date);
-                $('#con').append('Current condition: ' + data.weather[0].main);
-                $('#temp').append('Temperature: ' + data.main.temp + ' ' + String.fromCharCode(176) + 'F');
-                $('#humidity').append('Humidity: ' + data.main.humidity + '%');
-                $('#windSpeed').append('Wind Speed: ' + data.wind.speed + ' MPH');
+                var lat = data.coord.lat;
+                var lon = data.coord.lon;
+                console.log(lon);
+                //append result
+                $('#location').text(data.name);
+                $('#dateNow').text(date);
+                $('#con').text('Current condition: ' + data.weather[0].description);
+                $('#temp').text('Temperature: ' + (parseInt(data.main.temp).toFixed(1)) + ' ' + String.fromCharCode(176) + 'F');
+                $('#humidity').text('Humidity: ' + data.main.humidity + '%');
+                $('#windSpeed').text('Wind Speed: ' + data.wind.speed + ' MPH');
+                updateUvIndex(lat, lon);
 
 
             }
@@ -128,21 +155,76 @@ $(document).ready(function () {
 
     function getUvIndex(latt, longg) {
         var value;
-        console.log(apiKey);
+        return $.ajax({
+            url: 'http://api.openweathermap.org/data/2.5/uvi?' + apiKey + '&lat=' + latt + '&lon=' + longg,
+            method: "GET"
+        });
+
+    }
+    function updateUvIndex(latt, longg) {
+        var value;
         $.ajax({
             url: 'http://api.openweathermap.org/data/2.5/uvi?' + apiKey + '&lat=' + latt + '&lon=' + longg,
             method: "GET"
         }).then(function (response) {
-            value = response.value;
-            console.log(value);
-            return value;
+            $('#uV').text('UV Index: ' + response.value);
 
 
 
+        });
+
+    }
+
+    function fiveDayFor(userCity) {
+
+        var APIKey = '6ad58c387533b011c868da37071d9cec';
+        var fiveURL =
+            "http://api.openweathermap.org/data/2.5/forecast?appid=" +
+            APIKey +
+            "&q=" +
+            userCity +
+            "&units=imperial";
+
+        $.ajax({
+            url: fiveURL,
+            method: "GET"
+        }).then(function (response) {
+            var box = $("#forecastbox");
+            box.empty();
+            count = 1;
+
+            for (var x = 0; x < response.list.length; ++x) {
+                if (response.list[x].dt_txt.includes("00:00:00")) {
+                    iconObj = response.list[x].weather[0].icon;
+                    var daysForecast = $("<div>");
+                    var dayDate = $("<h6>");
+                    var dayImg = $("<img>");
+                    var tempForecast = $("<h6>");
+                    var humidityFor = $("<h6>");
+                    var iconURL = "https://openweathermap.org/img/w/" + iconObj + ".png";
+                    var today = new Date();
+                    var futureDate = new Date();
+                    futureDate.setDate(today.getDate() + count);
+                    var forecast = moment(futureDate).format("MM/D/YYYY");
+                    dayDate.text(forecast);
+                    dayImg.attr("src", iconURL);
+                    tempForecast.text("Temperature: " + response.list[x].main.temp + " °F ");
+                    humidityFor.text("Humidity: " + response.list[x].main.humidity + "% ");
+
+                    daysForecast.append(dayDate, dayImg, tempForecast, humidityFor);
+                    daysForecast.attr("class", "days");
+                    box.append(daysForecast);
+                    ++count;
+                }
+            }
         });
 
 
     }
 
 
+
+
+
 });
+
